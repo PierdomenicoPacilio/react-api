befadea02987051b05ed36a702eaac73ba418d48
PierdomenicoPacilio <piero.pacilio@gmail.com>
2025-04-11 16:42:38 +0200

    feat: male actors list and female actors list

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,25 @@ function App() {
   const maleActorsApi = 'https://www.freetestapi.com/api/v1/actors'
   const femaleActorsApi = 'https://www.freetestapi.com/api/v1/actresses'
 
-  const [actorsData, setActorsData] = useState([])
+  const [femaleActorsData, setFemaleActorsData] = useState([])
+  const [maleActorsData, setMaleActorsData] = useState([])
 
-  const getActors = () => {
-    axios.get(`${femaleActorsApi}`)
+  const getFemaleActors = () => {
+    axios.get(femaleActorsApi)
       .then(element => {
         console.log(element.data)
-        setActorsData(element.data)
+        setFemaleActorsData(element.data)
+      })
+      .catch(error => {
+        console.log(error)
+      })
+  }
+
+  const getMaleActors = () => {
+    axios.get(maleActorsApi)
+      .then(element => {
+        console.log(element.data)
+        setMaleActorsData(element.data)
       })
       .catch(error => {
         console.log(error)
@@ -24,9 +36,20 @@ function App() {
   return (
     <>
       <h1>Cast Fetching</h1>
-      <button onClick={getActors}>genera attori</button>
+      <button onClick={getFemaleActors}>genera attrici</button>
+      <button onClick={getMaleActors}>genera attori</button>
       <ul>
-        {actorsData.map(actor =>
+        {femaleActorsData.map(actor =>
+          <Card key={actor.id}
+            name={actor.name}
+            birth={actor.birth_year}
+            nazionality={actor.nazionality}
+            img={actor.image}
+            bio={actor.biography}
+            awards={actor.awards} />)}
+      </ul>
+      <ul>
+        {maleActorsData.map(actor =>
           <Card key={actor.id}
             name={actor.name}
             birth={actor.birth_year}
