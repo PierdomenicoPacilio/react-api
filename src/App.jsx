2487a37e528cbaf1ce8ed1017db2ebbf071282af
PierdomenicoPacilio <piero.pacilio@gmail.com>
2025-04-13 14:10:54 +0200

    fix: code adjust

--- src/App.jsx
+++ src/App.jsx
@@ -7,59 +7,33 @@ function App() {
   const maleActorsApi = 'https://www.freetestapi.com/api/v1/actors'
   const femaleActorsApi = 'https://www.freetestapi.com/api/v1/actresses'
 
-  const [femaleActorsData, setFemaleActorsData] = useState([])
-  const [maleActorsData, setMaleActorsData] = useState([])
-  const [AllActorsData, setAllActorsData] = useState([])
+  const [allActors, setAllActors] = useState([])
   const [selectedActor, setSelectedActor] = useState('')
   const [filteredActors, setFilteredActors] = useState([])
 
-  const getFemaleActors = () => {
-    axios.get(femaleActorsApi)
-      .then(element => {
-        console.log(element.data)
-        setFemaleActorsData(element.data)
-      })
-      .catch(error => {
-        console.log(error)
-      })
-  }
-
-  const getMaleActors = () => {
-    axios.get(maleActorsApi)
-      .then(element => {
-        console.log(element.data)
-        setMaleActorsData(element.data)
-      })
-      .catch(error => {
-        console.log(error)
-      })
-  }
-
-
-  // SCHIACCIARE DUE VOLTE IL BOTTONE PER FAR APPARIRE LA LISTA, MALEDETTA ASINCRONICITÁ
-  const getAllActors = () => {
-    getFemaleActors()
-    getMaleActors()
-    const sortedActorsData = [...femaleActorsData, ...maleActorsData].sort((a, b) =>
-      a.name.localeCompare(b.name)
-    )
-    setAllActorsData(sortedActorsData)
-  }
-
-
 
   useEffect(() => {
-    let currentActors = AllActorsData
-    if (selectedActor !== '') {
-      currentActors = AllActorsData.filter(actor =>
-        actor.name.toLowerCase().includes(selectedActor.toLowerCase())
-      )
-    }
-    setFilteredActors(currentActors)
-  }, [selectedActor, AllActorsData])
+    // PROMISE.ALL CHIAMA LE DUE API CONTEMPORANEAMENTE
+    Promise.all([axios.get(femaleActorsApi), axios.get(maleActorsApi)])
+      // PRENDE LE DUE RISPOSTE CONTEMPORANEAMENTE
+      .then(([femaleResponse, maleResponse]) => {
+        // LE METTE IN UN ARRAY E LO MOSTRA IN PAGINA IN ORDINE ALFABETICO
+        const allActorsData = [...femaleResponse.data, ...maleResponse.data];
+        setAllActors(allActorsData.sort((a, b) => a.name.localeCompare(b.name)))
+      })
+      .catch(error => {
+        console.log(error)
+      })
+  }, [])
 
 
-
+  // FILTRO
+  useEffect(() => {
+    let filtered = allActors.filter(actor =>
+      actor.name.toLowerCase().includes(selectedActor.toLowerCase())
+    )
+    setFilteredActors(filtered)
+  }, [selectedActor, allActors])
 
   return (
     <>
@@ -67,50 +41,24 @@ function App() {
         <h1>Cast Fetching</h1>
         <input
           type="text"
+          placeholder="Cerca attore o attrice..."
           value={selectedActor}
-          onChange={(e) => {
-            e.preventDefault()
-            setSelectedActor(e.target.value)
-          }} />
+          onChange={(e) => setSelectedActor(e.target.value)}
+        />
       </div>
 
-
-      {/* <button onClick={getFemaleActors}>genera attrici</button>
-      <button onClick={getMaleActors}>genera attori</button>
-
-      <div>
-        <ul>
-          {femaleActorsData.map(actor =>
-            <Card key={actor.id}
-              name={actor.name}
-              birth={actor.birth_year}
-              nazionality={actor.nazionality}
-              img={actor.image}
-              bio={actor.biography}
-              awards={actor.awards} />)}
-        </ul>
-        <ul>
-          {maleActorsData.map(actor =>
-            <Card key={actor.id}
-              name={actor.name}
-              birth={actor.birth_year}
-              nazionality={actor.nazionality}
-              img={actor.image}
-              bio={actor.biography}
-              awards={actor.awards} />)}
-        </ul>
-      </div> */}
-
-      <button onClick={getAllActors}>genera tutti gli attori</button>
       <ul>
-        {filteredActors.map((actor, index) =>
-          <Card key={index}
+        {filteredActors.map((actor, index) => (
+          <Card
+            key={index}
             name={actor.name}
             birth={actor.birth_year}
             nazionality={actor.nazionality}
             img={actor.image}
             bio={actor.biography}
-            awards={actor.awards} />)}
+            awards={actor.awards}
+          />
+        ))}
       </ul>
     </>
   )
